73bf6b1f5077116158faaa0c6d9ddd053cd84ba1
Shaung Cheng <shaungc@umich.edu>
2018-12-13 02:31:22 -0500

    refine part 3 fade in fade out

--- part-03-nba-player-model/part-03-nba-player-model.js
+++ part-03-nba-player-model/part-03-nba-player-model.js
@@ -36,8 +36,12 @@ function part03EntryPoint() {
 }
 
 /** TODO: Add your functions here! */
+PART_03_TRANSITING = false;
 
 function changeimg() {
+
+    if (PART_03_TRANSITING) return
+
   for (let canvasDOM of $("canvas")) {
     let context = canvasDOM.getContext('2d')
     context.clearRect(0, 0, context.canvas.width, context.canvas.height)
@@ -138,12 +142,37 @@ function changeimg() {
     drawradar2(13);
     console.log(img);
   } else {
+      PART_03_TRANSITING = true
+      if ($(".part3-outputs").css("display") !== "none") {
+        $(".part3-outputs")
+        .css("display", "flex")
+        .fadeOut(400, () => {
+            $(".avatar--error-msg").fadeIn(500, () => {
+                PART_03_TRANSITING = false
+            })
+        })
+      }
+      else {
+        $(".avatar--error-msg").fadeIn(500, () => {
+            PART_03_TRANSITING = false
+        })
+      }
+    
     console.log("no such player")
-    alert("Sorry! No player match. Please try another options.");
   }
 }
 
 function drawradar(i) {
+    $(".avatar--error-msg").hide()
+
+    PART_03_TRANSITING = true
+    $(".part3-outputs")
+        .css("display", "flex")
+        .hide()
+        .fadeIn(800, () => {
+            PART_03_TRANSITING = false
+        })
+
   i = i - 1;
 
   var data = [
@@ -198,7 +227,7 @@ function drawradar(i) {
     options: options
   });
 
-  console.log(myRadarChart);
+//   console.log(myRadarChart);
 
 }
 
@@ -255,7 +284,7 @@ function drawradar2(j) {
     options: options
   });
 
-  console.log(myRadarChart);
+//   console.log(myRadarChart);
 
 }
 
